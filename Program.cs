c34a6e12020584a772361a440d1aa3406e18980e
Mikołaj <mikip1010.2001@o2.pl>
2022-01-16 15:45:29 +0100

    E04 commit

--- Program.cs
+++ Program.cs
@@ -4,8 +4,32 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static void Triangle(int linesQuantity)
+        {
+            for (int i = 0; i < linesQuantity; i++)
+            {
+                for (int j = 0; j < i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = linesQuantity-1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj liczbę linii: ");
+            int linesQuantity = Convert.ToInt32(Console.ReadLine());
+
+            Triangle(linesQuantity);
         }
     }
 }
