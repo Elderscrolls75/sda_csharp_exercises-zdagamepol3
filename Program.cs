3e0fc9ae72e7adcaefd46bda6d2741dc59b62e0c
Mikołaj <mikip1010.2001@o2.pl>
2022-01-16 14:43:57 +0100

    E02 commit

--- Program.cs
+++ Program.cs
@@ -4,8 +4,35 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static public void PowerOf(int baza, int potęga)
+        { 
+            int wynik = baza;
+
+            for (int i = 1; i < potęga; i++)
+            {
+                wynik = wynik * baza;
+            }
+
+            Console.WriteLine(wynik);
+        }
         static void Main(string[] args)
         {
+            int baza = Convert.ToInt32(Console.ReadLine());
+            int potęga = Convert.ToInt32(Console.ReadLine());
+
+            if (potęga == 0)
+            {
+                Console.WriteLine("Wynik jest równy 1");
+            }
+            else if (baza == 0)
+            {
+                Console.WriteLine("Wynik równy jest 0");
+
+            }
+            else
+            {
+                PowerOf(baza, potęga);
+            }
         }
     }
 }
