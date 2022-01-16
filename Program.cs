9ba16a790e29fb817170352724967b3209ba8856
Mikołaj <mikip1010.2001@o2.pl>
2022-01-16 14:19:39 +0100

    E01 commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+
+            static void Cennik()
+            {
+                int cena = Convert.ToInt32(Console.ReadLine());
+                int liczbaLinii = Convert.ToInt32(Console.ReadLine());
+
+                for (int k = 1; k < liczbaLinii+1; k++)
+                {
+                    Console.WriteLine("Liczba sztuk: " + k + " łączna cena: " + k * cena);
+                }
+            }
+
+            Cennik();
         }
     }
 }
