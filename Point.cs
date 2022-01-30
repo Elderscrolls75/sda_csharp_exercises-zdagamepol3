165a1c2bcc5038ff8dd10008b369310921b02ab2
Mikołaj <mikip1010.2001@o2.pl>
2022-01-30 11:46:07 +0100

    E34 commit

---
+++ Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Point
+    {
+        protected int X { get; set; }
+        protected int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int moveX, int moveY)
+        {
+            X += moveX;
+            Y += moveY;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Obecne współrzędne punktu: ");
+            Console.WriteLine(X);
+            Console.WriteLine(Y);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Point punkt = new Point(5, 3);
+            punkt.Print();
+            punkt.Move(2, 5);
+            punkt.Print();
         }
     }
 }
