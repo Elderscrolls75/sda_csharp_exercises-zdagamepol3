3d9396ff7782a6a94c4ac3497e67d6d7ebbab498
Mikołaj <mikip1010.2001@o2.pl>
2022-01-30 14:09:25 +0100

    E37 commit

---
+++ Cuboid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Cuboid
+    {
+        public int Length { get; protected set; }
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+
+        public Cuboid(int length, int width, int height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double GetVolume()
+        {
+            double volume;
+            volume = Width * Length * Height;
+            return volume;
+        }
+
+        public static int CompareCuboids(Cuboid cuboid1, Cuboid cuboid2)
+        {
+            if (cuboid1.GetVolume() > cuboid2.GetVolume())
+            {
+                return 1;
+            }
+            else if (cuboid1.GetVolume() == cuboid2.GetVolume())
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,21 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Cuboid cuboid1 = new Cuboid(10, 12, 5);
+            Cuboid cuboid2 = new Cuboid(12, 4, 28);
+
+            if (Cuboid.CompareCuboids(cuboid1, cuboid2) == 1)
+            {
+                Console.WriteLine("Większa jest objętość pierwszego prostopadłościanu");
+            }
+            else if (Cuboid.CompareCuboids(cuboid1, cuboid2) == 0)
+            {
+                Console.WriteLine("Objętości prostopadłościanów są sobie równe");
+            }
+            else
+            {
+                Console.WriteLine("Większa jest objętość drugiego prostopadłościanu");
+            }
         }
     }
 }
