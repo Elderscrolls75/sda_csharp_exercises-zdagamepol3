0acf6b4b3c355eceba46ee37e9471bd93cfd6898
Mikołaj <mikip1010.2001@o2.pl>
2022-01-30 11:17:04 +0100

    E32

--- Program.cs
+++ Program.cs
@@ -7,9 +7,23 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
             Rectangle prostokąt = new Rectangle(5, 7);
+            Rectangle prostokąt1 = new Rectangle(1, 6);
 
             prostokąt.Show();
             Rectangle.PrintClassName();
+
+            Rectangle[] tablicaProstokątów = new Rectangle[3];
+
+            tablicaProstokątów[0] = prostokąt;
+            tablicaProstokątów[1] = prostokąt1;
+            tablicaProstokątów[2] = new Rectangle(2, 2);
+
+            foreach (Rectangle r in tablicaProstokątów)
+            {
+                r.Show();
+            }
+
+
         }
     }
 }
